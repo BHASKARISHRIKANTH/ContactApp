c649dfa4d9fa641c9492b483db6481a07027095a
BhaskariShrikanth <BhaskariShrikanth@MONO-77D35L2>
2024-11-01 21:52:53 +0530

    Add project files.

---
+++ ContactApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using ContactApp.Models;
+using ContactApp.Presentation;
+using ContactApp.Repository;
+
+namespace ContactApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Welcome To Contact App **********");
+            Console.WriteLine();
+            Console.WriteLine("Creating an Admin User ...");
+            User adminUser = new User(userid: 1, fname: "Admin", lname: "User", isadmin: true, isactive: true);
+            UserRepository.AddUser(adminUser);
+            Console.WriteLine("Admin user created with UserId: 1");
+            Console.WriteLine();
+            Console.WriteLine("Creating an Staff User...");
+            User adminUser1 = new User(userid: 2, fname: "Admin", lname: "User", isadmin: false, isactive: true);
+            UserRepository.AddUser(adminUser1);
+            Console.WriteLine("Staff user created with UserId: 2");
+            Console.WriteLine();
+            Menu menu = new Menu();
+            menu.ShowMainMenu();
+
+        }
+    }
+}
